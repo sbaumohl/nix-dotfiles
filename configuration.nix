e33b63b5b0efe64917ed8a1acf5307c2fcb9a27a
sbaumohl <mail@sambaumohl.me>
2024-04-25 02:09:08 -0500

    push new config

---
+++ configuration.nix
@@ -0,0 +1,217 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  users.defaultUserShell = pkgs.zsh;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      <home-manager/nixos>
+    ];
+
+  # Bootloader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  
+  # Use the GRUB 2 boot loader.
+  # boot.loader.grub.enable = true;
+  # boot.loader.grub.version = 2;
+  # boot.loader.grub.efiSupport = true;
+  boot.loader.grub.useOSProber = true;
+  boot.loader.grub.device = "nodev";
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Chicago";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # graphics
+  services.xserver = {
+    enable = true;
+    # resolutions = [{ x = 1920; y = 1080; }];
+    # virtualScreen = { x = 1920; y = 1080; };
+    layout = "us"; # keyboard layout
+    xkbVariant = "";
+    desktopManager = {
+      cinnamon.enable = true;
+    };
+
+    displayManager.defaultSession = "cinnamon";
+    autorun = true; # run on graphic interface startup
+    # libinput.enable = true; # touchpad support
+    videoDrivers = [ "nvidia" ];
+  };
+
+  # graphics - wayland
+  # programs.sway.enable = true;
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.mizu = {
+    isNormalUser = true;
+    description = "Sam";
+    extraGroups = [ "networkmanager" "wheel" ];
+  };
+
+  # Home Manager
+
+  # TODO vim config, turn into seperate .nix file
+  home-manager.users.mizu = { config, pkgs, ... }: {
+    # home.packages = [ pkgs.dnsutils ];
+    home.stateVersion = "23.11";
+
+    programs = {
+    	zsh = {
+    	enable = true;
+	enableCompletion = true;
+    	enableAutosuggestions = true;
+	syntaxHighlighting.enable = true;
+	shellAliases = {
+		ll = "ls -l";
+		update = "sudo nixos-rebuild switch";
+		clean = "nix store gc";
+	};
+	history.size = 10000;
+	history.path = "${config.xdg.dataHome}/zsh/history";
+
+	oh-my-zsh = {
+		enable = true;
+		plugins = [ "git" "rust" "sudo" "copypath" "copybuffer" "jsontools" ];
+		theme = "robbyrussell";
+	};
+
+	};
+    };
+ 
+  };
+
+  programs.zsh.enable = true;
+ 
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+    # CLI Tools
+    wget
+    curl
+    tmux
+    git
+    gh
+    
+    # C-Programming
+    gcc
+    clang
+
+    # RUST
+    rustc
+    rustup
+
+    # OS Prober
+    os-prober
+
+    # Applications
+    firefox
+    bitwarden
+    thunderbird
+    protonvpn-gui
+    discord
+
+    # IDEs/Tools
+    neovim
+    vscode
+  ];
+
+  fonts = {
+    packages = with pkgs; [
+      monaspace
+    ];
+
+    fontconfig = {
+      defaultFonts = {
+        monospace = [ "'Monaspace Argon'" ];
+      };
+    };
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+}
